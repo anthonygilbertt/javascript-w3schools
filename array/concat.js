fab9afa9230a5935a732588be12ce91755f6eb7f
gilby <anthony.gilbert93@gmail.com>
2020-05-17 02:47:12 -0500

    Learned concat() method

---
+++ array/concat.js
@@ -0,0 +1,7 @@
+//Concat() - Joins 2 or more arraus, and returns a copy of the joined arrays
+
+var hege = ["Cecilie", "Lone"];
+var stale = ["Emile", "Tobias", "Linus"];
+var children = hege.concat(stale);
+
+console.log(children);
